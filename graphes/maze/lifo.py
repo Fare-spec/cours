bf091181e42e9ed617bd2371d57f752a5981058c
Spectre <elouan.fare@protonmail.ch>
2025-01-27 09:51:20 +0100

    Auto urgent commit.

---
+++ graphes/maze/lifo.py
@@ -0,0 +1,21 @@
+class Queue:
+    def __init__(self) -> None:
+        self.element = []
+
+    def enfiler(self,element):
+        self.element.append(element)
+
+    def est_vide(self):
+        return len(self.element) == 0
+
+    def defiler(self):
+        assert self.est_vide(), "La file est vide"
+        return self.element.pop(0)
+
+    def size(self):
+        return len(self.element)
+
+    def index(self, k):
+        assert self.est_vide() , "La file est vide"
+        return self.element[k]
+
