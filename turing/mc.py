a1ed22e8dde0acf930891becb31718629354ad5e
Spectre <elouan.fare@protonmail.ch>
2025-04-02 08:04:13 +0200

    Auto urgent commit.

--- turing/mc.py
+++ turing/mc.py
@@ -1,19 +1,21 @@
-import Tabidir, exemples
+import bitab as Tabidir
+import exemples
 class Configuration:
     def __init__(self, ruban,position, etat_courant):
         self.ruban = ruban
-        self.position = position
+        self.position = position:wq
+        
         self.etat_courant = etat_courant
 
     def _str__(self):
         chaine = ' '
         chaine = chaine +(self.position-ruban.imin())*2*' '+self.etat_courant
         chaine = chaine + ' '+ (self.position+ruban.imin()*2*' '+ '|'+'\n')
-        chaine = chaine+slef.ruban.__str__()
+        chaine = chaine+self.ruban.__str__()
         return chaine
 
 if __name__ == "__main__":
-    ruban = Ta
+    ruban = Tabidir
 
 
 class Machine:
